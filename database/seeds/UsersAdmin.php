4d9597ad2f145864e16cd5d815bdd8dc3f47cb50
only1Migwi <charliemigwi@gmail.com>
2021-05-25 09:18:15 +0300

    changed useradminseeder

--- database/seeds/UsersAdmin.php
+++ database/seeds/UsersAdmin.php
@@ -4,20 +4,38 @@ use Illuminate\Database\Seeder;
 use App\User;
 
 class UsersAdmin extends Seeder
+// {
+//     /**
+//      * Run the database seeds.
+//      *
+//      * @return void
+//      */
+//     public function run()
+//     {
+//         User::where('id',1)->delete();//To avoid duplicate user accounts
+//         $user = new User();
+//         $user->id = 1;
+//         $user->name = env('USER_NAME','Lenard Mangay-ayam');
+//         $user->email = env('USER_EMAIL','lenard.mangayayam@voting-system.com');
+//         $user->password = bcrypt(env('USER_PASS','admin'));
+//         $user->save();
+//     }
+// }
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        User::where('id',1)->delete();//To avoid duplicate user accounts
-        $user = new User();
-        $user->id = 1;
-        $user->name = env('USER_NAME','Lenard Mangay-ayam');
-        $user->email = env('USER_EMAIL','lenard.mangayayam@voting-system.com');
-        $user->password = bcrypt(env('USER_PASS','admin'));
-        $user->save();
+        $users = [
+            [
+                'id'             => 1,
+                'name'           => 'Admin',
+                'email'          => 'admin@admin.com',
+                'password'       => '$2y$10$7Xn/8bPJ89ypj0cIxwoH9OOXnbK/.9xrLfFh2G4LUSRkw6j7Agn0K',
+                'remember_token' => null,
+                'created_at'     => '2019-09-24 19:16:02',
+                'updated_at'     => '2019-09-24 19:16:02',
+            ],
+        ];
+
+        User::insert($users);
     }
 }
